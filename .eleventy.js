bada70b718706224a531db4ac8e5d8dc54d93686
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2023-03-22 00:25:05 -0400

    scss and extra pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,20 @@
+const sass = require("sass")
+
 module.exports = function (eleventyConfig){
+    eleventyConfig.addTemplateFormats("scss");
+
+    eleventyConfig.addExtension("scss", {
+        outputFileExtension: "css",
+
+        compile: async function(inputContent) {
+            let result = sass.compileString(inputContent);
+
+            return async (data) => {
+                return result.css
+            }
+        }
+    })
+
     return {
         dir: {
             input: 'src',
--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Doing my portfolio in 11ty
 
 checklist:
 [] fill out content for each page
-[] set up navigation
+[x] set up navigation
 [] set up front matter in each page
 [] set up blog and pagination
-[] set up css(using sass for this project)
+[x] set up css(using sass for this project)
 [] set up scss vars
 [] light and dark mode
