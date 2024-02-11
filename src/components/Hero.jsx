caecbcf9c4e95685d2448c45e84f03b0e285ff13
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2024-02-11 01:05:05 -0700

    update:added a starter component and jsx and css

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+function HeroComponent() {
+    return(
+        <div className="hero">
+            <h1 className="hero__title">Hello World!</h1>
+        </div>
+    )
+}
+
+export default HeroComponent
