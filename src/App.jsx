d9bd906c35f25a4d197590e2001041e2306bbfa0
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2024-04-10 17:51:20 -0600

    update:added to the css and fixed the img container

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
+import { useState } from 'react'
 import './App.css'
+import myImg from './assets/IMG_2178.png'
 
 
 function App() {
-
+  const [current, currentClicked] = useState('about')
   return (
     <>
-      <h1> DenzelEchevarria.dev </h1>
+      <div className='img-container'>
+        <img src={myImg} alt="ME" />
+      </div>
+
       <nav>
 
         <ul>
           <li>
-            <a href="/" onClick={"#"}>About</a>
+            <button  onClick={()=>currentClicked('about')}>About</button>
           </li>
 
           <li>
-            <a href="/" onClick={"#"}>Projects</a>
+            <button  onClick={()=>currentClicked('projects')}>Projects</button>
           </li>
 
           <li>
-            <a href="/about" onClick={"#"}>Blog</a>
+            <button  onClick={()=>currentClicked('Blog')}>Blog</button>
           </li>
         </ul>
 
       </nav>
+
+      <div className='switch-div'>
+
+        <div className={current === "about" ? "display" : "hidden"}>
+        <p>With a <em>strong affinity for technology</em> I dove head first into the community, traveling to participate in events with the goal of growing my community presence and developing my skillset. Having formed strong bonds through the community I became an internet personality on streaming platforms, merging different communities with my own. While using OBS I grew intrigued by web extensions, manipulating my stream overlays for a professional aesthetic. The knowledge gained allowed me to <em>give back to the community</em> and aid others to improve their streaming quality. After a while I found myself interested in open source software, and before I knew it I was diving into web development, ready to share my personality and spread joy to my surroundings.</p>
+        </div>
+
+        <div className={current === "projects" ? "display" : "hidden"}>
+          <p>projects</p>
+        </div>
+
+        <div className={current === "blog" ? "display" : "hidden"}>
+          <p>blog</p>
+        </div>
+
+      </div>
     </>
     
   )
